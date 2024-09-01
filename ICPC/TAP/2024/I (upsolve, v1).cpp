65625c8443f1c47839ef514306590fbacc5262d0
Ariel Leonardo Fideleff <sefide77@gmail.com>
2024-09-01 15:11:31 -0300

    Added new solution for TAP

---
+++ ICPC/TAP/2024/I (upsolve, v1).cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+ 
+#define forn(i,n) for(int i = 0; i < int(n); i++)
+#define forsn(i,s,n) for(int i = int(s); i < int(n); i++)
+#define dforn(i,n) for(int i = int(n)-1; i >= 0; i--)
+#define dforsn(i,s,n) for(int i = int(n)-1; i >= int(s); i--)
+#define fst first
+#define snd second
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define FAST_IO ios::sync_with_stdio(false);cin.tie(nullptr)
+ 
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef long double ld;
+
+int const MAXN = 1005;
+int const MAXG = MAXN*MAXN + 2*MAXN;
+
+bool board[MAXN][MAXN];
+vi G[MAXG];
+int deg[MAXG];
+bitset<MAXG> done;
+int rta[MAXN][MAXN];
+
+struct edge {
+	int y;
+	list<edge>::iterator rev;
+	edge(int y):y(y){}
+};
+list<edge> g[MAXG];
+void add_edge(int a, int b){
+	g[a].push_front(edge(b));auto ia=g[a].begin();
+	g[b].push_front(edge(a));auto ib=g[b].begin();
+	ia->rev=ib;ib->rev=ia;
+}
+vector<int> p;
+void go(int x){
+	while(g[x].size()){
+		int y=g[x].front().y;
+		g[y].erase(g[x].front().rev);
+		g[x].pop_front();
+		go(y);
+	}
+	p.push_back(x);
+}
+vector<int> get_path(int x){ // get a path that begins in x
+// check that a path exists from x before calling to get_path!
+	p.clear();go(x);reverse(p.begin(),p.end());
+	return p;
+}
+
+void check(int st) {
+    done[st] = true;
+    for (auto &i : G[st])
+        if(!done[i]) check(i);
+}
+
+int main() {
+    FAST_IO;
+
+    int n,m; cin >> n >> m;
+    forn(i,n) forn(j,m) {
+        char x; cin >> x;
+        if (x == '.') board[i][j] = true;
+    }
+
+    forn(i,n) forn(j,m)
+        if (board[i][j]) {
+            int me = m*i + j;
+            int row = n*m + i;
+            int col = n*m + n + j;
+            add_edge(row,me);
+            add_edge(col,me);
+            G[row].pb(me), G[me].pb(row);
+            G[col].pb(me), G[me].pb(col);
+            deg[me] += 2, deg[row]++, deg[col]++;
+        }
+
+    vi odd;
+    forn(i,MAXG) if (deg[i]&1) odd.pb(i);
+
+    if ((int)odd.size() > 2)
+        return cout << "*\n", 0;
+
+    forn(i,MAXG)
+        if (deg[i] > 0) {
+            check(i); break;
+        }
+    forn(i,MAXG) 
+        if (deg[i] > 0 && !done[i])
+            return cout << "*\n", 0;
+
+    vi path;
+    if (!odd.empty()) {
+        int a = odd[0], b = odd[1];
+        add_edge(a,b);
+        path = get_path(a);
+    }
+    else {
+        int auxi = -1;
+        forsn(i,n*m,MAXG)
+            if (deg[i] > 0) {
+                auxi = i; break;
+            }
+        if (auxi != -1)
+            path = get_path(auxi);
+    }
+
+    int cnt = 1;
+    for (auto &i : path) {
+        if (i < n*m) {
+            rta[i/m][i%m] = cnt++;
+        }
+    }
+
+    forn(i,n) forn(j,m)
+        cout << rta[i][j] << " \n"[j == m-1];
+
+    return 0;
+}
