0bdc39344d2de7f1e06ae1aaedc653b91a75ccca
Ariel Leonardo Fideleff <sefide77@gmail.com>
2026-04-12 18:15:04 -0300

    April Fools CF problem solution

---
+++ CodeForces/2214J.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s; 
+    getline(cin, s);
+    if (s == "Are you a verified human?")
+        cout << "Yes, I can attest to my status as a thoroughly validated, carbon-based biological entity.\n";
+    else cout << "Yes\n";
+    
+    return 0;
+}
