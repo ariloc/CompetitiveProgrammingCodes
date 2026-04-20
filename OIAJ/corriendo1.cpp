0663ad745d67c2fea76204f8fc9c91270312ae45
Ariel Leonardo Fideleff <sefide77@gmail.com>
2026-04-20 20:08:08 -0300

    New random OIA solution

---
+++ OIAJ/corriendo1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+#define forn(i,n) for(int i = 0; i < int(n); i++)
+#define forsn(i,s,n) for(int i = int(s); i < int(n); i++)
+#define dforn(i,n) for(int i = int(n)-1; i >= 0; i--)
+#define dforsn(i,s,n) for(int i = int(n)-1; i >= int(s); i--)
+#define fst first
+#define snd second
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define FAST_IO ios::sync_with_stdio(false);cin.tie(nullptr)
+
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef long double ld;
+
+vi corriendo(int T, int K, int N) {
+    int vueltas = N/T;
+    int maxi = K*vueltas + min(N%T,K);
+    int mini = K*vueltas + min(K, max(0, K-(T-N%T)));
+    return {mini, maxi};
+}
