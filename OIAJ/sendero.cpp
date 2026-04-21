476baffc273d2707fcc4ca428640373a4883266a
Ariel Leonardo Fideleff <sefide77@gmail.com>
2026-04-21 03:26:22 -0300

    New solution for sendero from OIAJ

--- OIAJ/sendero.cpp
+++ OIAJ/sendero.cpp
@@ -1,84 +1,72 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
-#define forn(i,n) for(int i=0;i<int(n);i++)
+#define forn(i,n) for(int i = 0; i < int(n); i++)
+#define forsn(i,s,n) for(int i = int(s); i < int(n); i++)
+#define dforn(i,n) for(int i = int(n)-1; i >= 0; i--)
+#define dforsn(i,s,n) for(int i = int(n)-1; i >= int(s); i--)
+#define fst first
+#define snd second
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define FAST_IO ios::sync_with_stdio(false);cin.tie(nullptr)
 
 using namespace std;
-typedef pair<int, char> ic;
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef long double ld;
 
-map<char, int> pri;
-ic p[3];
-int r = 0;
+int const INF = 2e9+10;
+int const MAXA = 26;
+int const MAXN = 2e6;
+int const MAP[] = {'B', 'G', 'N'};
 
-void lineal (int n, int pos, string &baldosas) {
-    for (int i = pos; i < pos+n; i++) {
-        forn (j,3) {
-            if ((i-1 >= 0 ? baldosas[i-1] != p[j].second : true) and (i+1 < baldosas.size() ? baldosas[i+1] != p[j].second : true)) {
-                baldosas[i] = p[j].second;
-                r+=p[j].first;
-                break;
+int dp[MAXN][3];
+int P[MAXN][3];
+
+int sendero(int B, int G, int N, string &s) {
+    int n = (int)s.size();
+    int costs[] = {B,G,N};
+
+    forn(i,n) forn(k,3) dp[i][k] = INF, P[i][k] = -1;
+
+    forn(i,3) if (s[0] == 'R') dp[0][i] = costs[i];
+    forn(i,3) if (s[0] == MAP[i]) dp[0][i] = 0;
+    forsn(i,1,n) {
+        if (s[i] == 'R') {
+            forn(j,3) forn(k,3) {
+                if (j == k) continue;
+                int cost = dp[i-1][j] + costs[k];
+                if (cost < dp[i][k]) {
+                    dp[i][k] = cost;
+                    P[i][k] = j;
+                }
             }
         }
-    }
-}
-
-void simetrico (int n, int pos, string &baldosas) {
-    int st;
-    if (pri[baldosas[pos-1]] > pri[baldosas[pos+n]]) {
-        for (int i = pos+n-1; i > pos-1; i--) {
-            forn (j,3) {
-                if ((i-1 >= 0 ? baldosas[i-1] != p[j].second : true) and (i+1 < baldosas.size() ? baldosas[i+1] != p[j].second : true)) {
-                    baldosas[i] = p[j].second;
-                    r+=p[j].first;
-                    break;
+        else {
+            forn(j,3) {
+                int ind = s[i] == 'B' ? 0 : s[i] == 'G' ? 1 : 2;
+                if (j == ind) continue;
+                if (dp[i-1][j] < dp[i][ind]) {
+                    dp[i][ind] = dp[i-1][j];
+                    P[i][ind] = j;
                 }
             }
         }
     }
-    else {
-        for (int i = pos; i < pos+n; i++) {
-            forn (j,3) {
-                if ((i-1 >= 0 ? baldosas[i-1] != p[j].second : true) and (i+1 < baldosas.size() ? baldosas[i+1] != p[j].second : true)) {
-                    baldosas[i] = p[j].second;
-                    r+=p[j].first;
-                    break;
-                }
-            }
-        }
-    }
-}
 
-int sendero(int B, int G, int N, string &baldosas)
-{
-    p[0] = {B,'B'};
-    p[1] = {G,'G'};
-    p[2] = {N,'N'};
-    int cnt = 0;
-    bool bl = false;
-    sort(p,p+3);
-    forn (i,3) {
-        pri[p[i].second] = p[i].first;
-    }
-    forn (i, baldosas.size()+1) {
-        char b = baldosas[i];
-        if (b == 'R' and i != baldosas.size()) {
-            bl = true;
-            cnt++;
-        }
-        else if (bl) {
-            if (cnt%2) {
-                pair <char,char> desc = {b,baldosas[i-cnt-1]};
-                if (pri[desc.first] - pri[desc.second] == 1 or cnt == 1 or baldosas[i-cnt-1] == baldosas[i]) {
-                    lineal(cnt, i-cnt, baldosas);
-                }
-                else simetrico(cnt, i-cnt, baldosas);
+    int mini = INF, ind;
+    forn(k,3) if (dp[n-1][k] < mini) mini = dp[n-1][k], ind = k;
 
-            }
-            else {
-                lineal(cnt, i-cnt, baldosas);
-            }
-            cnt = 0;
-            bl = false;
-        }
+    string auxi;
+    int j = n-1;
+    while (j >= 0) {
+        auxi.pb(MAP[ind]);
+        ind = P[j][ind];
+        --j;
     }
-    return r;
+    reverse(all(auxi));
+    swap(s,auxi);
+
+    return mini;
 }
