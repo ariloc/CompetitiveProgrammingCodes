252ae89fb9bd985bac964fddc9f0ada5ed0da860
Ariel Leonardo Fideleff <sefide77@gmail.com>
2026-06-18 12:25:28 -0300

    Fix old solution from OIAJ

--- OIAJ/pajaros.cpp
+++ OIAJ/pajaros.cpp
@@ -1,38 +1,54 @@
 #include <bits/stdc++.h>
 
 #define forn(i,n) for(int i = 0; i < int(n); i++)
+#define forsn(i,s,n) for(int i = int(s); i < int(n); i++)
+#define dforn(i,n) for (int i = int(n)-1; i >= 0; i--)
+#define dforsn(i,s,n) for(int i = int(n)-1; i >= int(s); i--)
+#define all(c) (c).begin(),(c).end()
+#define fst first
+#define snd second
+#define FAST_IO ios::sync_with_stdio(false);cin.tie(nullptr);
 
 using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int,int> ii;
 
-// areaT = 1/2 | areaC = 1
-float areaPaj = 0, areaP = 1/2 + 1;
-int h = 0, tot = 0;
-vector<int> alts;
+const int MAXM = 6e4+2;
+const ll INF = 1e12+5;
+const int MAXD = 301;
+
+int vuelo[MAXM];
+ll dp[MAXM][MAXD];
+int S[MAXM][MAXD];
+int N;
+
+ll solve(int act, int H) {
+    if (H < 0) return INF;
+    if (dp[act][H]) return dp[act][H];
+    if (act == N+1) return 1;
+    dp[act][H] = min(solve(act+1,H+1),solve(act+1,H-1)) + abs(H-vuelo[act]);
+    ll U = solve(act+1,H+1),D = solve(act+1,H-1);
+    if (min(U,D) == U)
+        S[act][H] = H+1;
+    else S[act][H] = H-1;
+    return dp[act][H];
+}
 
 int main() {
     freopen("pajaros.in","r",stdin);
+    freopen("pajaros.out","w",stdout);
 
-    int n, ant = 0;
-    cin >> n;
-    forn (i,n) {
+    scanf("%d",&N);
+
+    vuelo[0] = 0;
+    forn (i,N) {
         int x;
-        cin >> x;
-        areaPaj += (ant+x)/(float)2;
-        ant = x;
-        alts.push_back(x);
+        scanf("%d",&x);
+        vuelo[i+1] = x;
     }
-    forn (i,n) {
-        if (areaP < areaPaj) {
-            h++;
-            areaP += 1/2 + n-i-1;
-        }
-        else {
-            h--;
-            areaP -= 1/2 + n-i-1;
-        }
-        tot += abs(h-alts[i]);
-        cout << h << endl;
-    }
-    cout << tot;
+
+    printf("%lld\n",solve(0,0)-1);
+
     return 0;
 }
